7f99c9572c9a694295594b88b18a9404139a85ec
saknet <andreas.andra@helsinki.fi>
2022-10-11 22:38:55 +0300

    init

---
+++ cmd/findNearest.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"math"
+)
+
+type Feature_data struct {
+	Latitude  float64
+	Longitude float64
+	Key       string
+}
+
+func FindNearestFeature(data FindNearestInput) string {
+	closestDistance := 9999999999.9
+	closest := data.Feature_data[0].Key
+
+	for i := 0; i < len(data.Feature_data); i++ {
+
+		distance := getDistance(data.Point_Longitude, data.Point_latitude, data.Feature_data[i].Longitude, data.Feature_data[i].Latitude)
+		if distance < closestDistance {
+			closestDistance = distance
+			closest = data.Feature_data[0].Key
+
+		}
+
+	}
+
+	return closest
+}
+
+func getDistance(x1 float64, y1 float64, x2 float64, y2 float64) float64 {
+	lon1 := x1 * math.Pi / 180
+	lon2 := x2 * math.Pi / 180
+	lat1 := y1 * math.Pi / 180
+	lat2 := y2 * math.Pi / 180
+
+	dlon := lon2 - lon1
+	dlat := lat2 - lat1
+
+	a := math.Pow(math.Sin(dlat/2), 2) + math.Cos(lat1)*math.Cos(lat2)*math.Pow(math.Sin(dlon/2), 2)
+
+	c := 2 * math.Asin(math.Sqrt(a))
+
+	//  c * Radius of earth in meters
+
+	return c * 6371000
+
+}
